451c310fae2dbbc548f8fa128d4ddb94cd46f8e1
patrlckroe <pat10roe@icloud.com>
2025-01-18 19:27:32 -0600

    Track large files over 10 MB with Git LFS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,3 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+version https://git-lfs.github.com/spec/v1
+oid sha256:3de4ba23ff1f687685651cad622700613998de80daa30ac4d82c47776a04019d
+size 393
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,3 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+version https://git-lfs.github.com/spec/v1
+oid sha256:223f2def401123e32ac5664f79c73bdaa8b3a77caf466a304bff919a2a28c16f
+size 393
