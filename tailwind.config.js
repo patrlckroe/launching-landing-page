7a07aa31260466abc4b4b595bffe2f3efdfa61cd
patrlckroe <pat10roe@icloud.com>
2025-01-19 15:20:33 -0600

    Build successful and ready for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the `src` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
