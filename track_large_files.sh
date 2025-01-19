b04811f4e65236e7fef3c3d38a252af6167a6c49
patrlckroe <pat10roe@icloud.com>
2025-01-19 13:15:07 -0600

    Update

--- track_large_files.sh
+++ track_large_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Set the file size threshold (10 MB in bytes)
+SIZE_THRESHOLD=10485760
+
 # Find files larger than 10 MB and track them with Git LFS
-find . -type f -size +10M | while read -r file; do
+find . -type f -size +"$((SIZE_THRESHOLD))"c | while read -r file; do
     git lfs track "$file"
     echo "Tracking $file with Git LFS"
 done
