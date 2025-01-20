c13f3af080da3261aa3f00b6ad6185ff6e10992e
patrlckroe <pat10roe@icloud.com>
2025-01-19 21:18:12 -0600

    Visual editor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the `src` directory
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all relevant files
   ],
   theme: {
     extend: {
